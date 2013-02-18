3e0cc49ae0c8bc4b4d4dc4256288fca500fd2f19
erlon <erlonlon@gmail.com>
2013-02-18 17:55:14 -0300

    primeiro upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Erloncarlos::Application.config.secret_token = 'f068cec745b226991e95835a93081406d155c3b0382a9259c3653d1bbda868183bfb2fb3f72d0e12262e24a866ddd346ba272bf61c852b4b6cdcf773c772c961'
